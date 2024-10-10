2bdff9ab752bed23b061a6d5cc5ba0cbeaabe828
Owen Smith <ldp520@york.ac.uk>
2024-10-10 17:54:01 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+}
+
+group = "io.github.uoyteamsix"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("com.badlogicgames.gdx:gdx:1.12.1")
+    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.12.1")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-desktop")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/io/github/uoyteamsix/Game.java
@@ -0,0 +1,7 @@
+package io.github.uoyteamsix;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Game extends ApplicationAdapter {
+
+}
